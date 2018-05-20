e6a1934de31575f5cac208d795ba1d39034f6413
Jose Gómez <josegomezgadea@gmail.com>
2018-05-20 14:01:53 +0200

    Segunda ampliación hecha, snipped casi arreglado y muchos cambios menores

--- SAR_search.py
+++ SAR_search.py
@@ -51,16 +51,16 @@ def listOfNotices(filename):
             listaProcesada.append(element+"</DOC>")
     return listaProcesada[0:-1]
 
-def imprimir(postingResultante,docid,word):
+def imprimir(postingResultante,docid,words):
     """
     Imprime por consola el resultado de la busqueda de los términosself.
     :param postingResultante: posting List resultado de las operaciones sobre los términos (AND OR NOT)
     :param docid: diccionario donde para cada docid tenemos el documento al que hace referencia
-    :param word: *****REVISAR!****** word para la cual se calcula el snipped
+    :param words: palabras para las cuales se calcula el snipped
     """
     if len(postingResultante) <= 0:
         print("\n------------------------------------")
-        print("No hay resultados")
+        print("\nNo hay resultados")
         print("\n------------------------------------")
     if len(postingResultante) <= 2:
         for value in postingResultante:
@@ -86,7 +86,9 @@ def imprimir(postingResultante,docid,word):
             titulo = root.find("TITLE").text
             print("\n------------------------------------")
             print("\nTítulo: \n %s" %clean_text(titulo))
-            texto_result = snipped(word,clean_text(texto))
+            texto_result = ""
+            for w in words:
+                texto_result = texto_result + "\n" + snipped(w,clean_text(texto))
             #print("hola" + texto)
             print("\nNoticia: \n %s" %texto_result)
     else:
@@ -180,24 +182,27 @@ def operadores(post1,post2,operador):
 def getIndex(word):
     """
     Ampliación 2: obtiene las referencias de los ficheros en la cual aparece
-    esa palabra para el campo indicado (headline,text,category,date)
+    esa palabra para el campo indicado (headline,text,category,date).
+    Supondremos que está bien escrito.
     """
     words = word.split(":")
     if len(words) == 1:
         return Index.get(word)
     else:
-        hacer switch
-            case words[0] == headline...
-                return IndexHeadLine.get(word)
-        etc...
-    # TODO: Terminar método
+        if words[0] == "headline":
+            return IndexHeadLine.get(words[1])
+        elif words[0] == "text":
+            return Index.get(words[1])
+        elif words[0] == "category":
+            return IndexCategory.get(words[1])
+        elif words[0] == "date":
+            return IndexDate.get(words[1])
 
 def andOrNot(consulta,Index,docID):
     """
     Ampliación 1: permitimos consultas del estilo term1 and term2 or term3 and not term4.
     Podemos suponer que las consultas están correctamente escritas.
     """
-    print("Ejecutando la primera ampliación")
     extension = []
     for i in range(0,len(consulta)):
         if i %2 != 0 and consulta[i] not in conectivas:
@@ -205,19 +210,17 @@ def andOrNot(consulta,Index,docID):
         else:
             extension.append(consulta[i])
     consulta = extension
-    print(consulta)
-    post1 = Index.get(consulta[0])
+    post1 = getIndex(consulta[0])
     operador = consulta[1]
-    post2 = Index.get(consulta[2])
+    post2 = getIndex(consulta[2])
 
     #print("term1:%s operador: %s term2: %s" %(consulta[0],consulta[1],consulta[2]))
     result = []
     if post1 is not None and post2 is not None or operador == "or":
         result = operadores(post1,post2,operador)
-        print(result)
     for i in range(3, len(consulta)-1):
         operador = consulta[i]
-        post2 = Index.get(consulta[i+1])
+        post2 = getIndex(consulta[i+1])
         #print(" operador: %s term2: %s" %(consulta[i],consulta[i+1]))
         if result is not None and post2 is not None or operador == "or":
             result = operadores(result,post2,operador)
@@ -246,23 +249,24 @@ if __name__ == '__main__':
             exit()
         consulta = consulta.lower()
         consulta_terms = consulta.split(" ")
-        if len(set(conectivas).intersection(consulta_terms)) >0:
+        # En caso de que haya alguna conectiva, entramos en el método andOrNot
+        if len(set(conectivas).intersection(consulta_terms)) > 0:
             andOrNot(consulta_terms,Index,docid)
         else:
             result  = []
             if len(consulta_terms) == 1:
-                #Si solo hay un terminos
-                result = Index.get(consulta_terms[0])
+                #Si solo hay un término
+                result = getIndex(consulta_terms[0])
                 if result is None:
                     result = []
             else:
-                post1 = Index.get(consulta_terms[0])
-                post2 = Index.get(consulta_terms[1])
+                post1 = getIndex(consulta_terms[0])
+                post2 = getIndex(consulta_terms[1])
                 if post1 is not None and post2 is not None:
                     result = interseccion(post1,post2)
                 for i in range(2, len(consulta_terms)):
-                    post2 = Index.get(consulta_terms[i])
+                    post2 = getIndex(consulta_terms[i])
                     if result is not None and post2 is not None:
                         result = interseccion(result,post2)
                 #Result tiene la interseccion de todos los terminos de la consulta
-            imprimir(result,docid,consulta_terms[0])
+            imprimir(result,docid,consulta_terms)
